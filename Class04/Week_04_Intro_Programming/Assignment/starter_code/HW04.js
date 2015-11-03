9d4be2f52ab39f7642018a7d74d26200a3286cf1
Sonya8641 <swilliams804@verizon.net>
2015-11-03 16:16:28 -0500

    Homework04 Relaxr JQuery
    Homework04 Relaxr JQuery

---
+++ Class04/Week_04_Intro_Programming/Assignment/starter_code/HW04.js
@@ -0,0 +1,28 @@
+$(document) .ready(function() {
+
+	$("p") .hide();
+
+	$("h1") .click(function() {
+		$(this).next().slideToggle(300);
+
+	)};
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
